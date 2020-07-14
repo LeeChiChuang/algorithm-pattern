cf921de6134875460bd3cadb8ec44151e2314a87
LeeChiChuang <leechichuang@163.com>
2020-07-14 23:00:53 +0800

    commit

--- src/binarytree/binary_tree_maximum_path_sum.go
+++ src/binarytree/binary_tree_maximum_path_sum.go
@@ -1,5 +1,7 @@
 package binarytree
 
+import "math"
+
 // 124. 二叉树中的最大路径和  https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/
 // 给定一个非空二叉树，返回其最大路径和。
 //
@@ -27,5 +29,30 @@ package binarytree
 // 输出: 42
 
 func MaxPathSum(root *TreeNode) int {
+	maxSum := math.MinInt32
+	var maxGain func(*TreeNode) int
 
+	maxGain = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		leftGain := max(maxGain(node.Left), 0)
+		rightGain := max(maxGain(node.Right), 0)
+		maxNewPath := node.Val + leftGain + rightGain
+
+		maxSum = max(maxSum, maxNewPath)
+
+		return node.Val + max(leftGain, rightGain)
+	}
+	maxGain(root)
+
+	return maxSum
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
 }
--- src/binarytree/binary_tree_maximum_path_sum_test.go
+++ src/binarytree/binary_tree_maximum_path_sum_test.go
@@ -1,1 +1,31 @@
 package binarytree
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMaxPathSum(t *testing.T) {
+	type args struct {
+		a *TreeNode
+	}
+
+	arg1 := CreateTree([]int{1, 2, 3}, &TreeNode{}, 0, 3)
+	arg2 := CreateTree([]int{-1, -1, -1}, &TreeNode{}, 0, 3)
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"t1", args{a: arg1}, 6},
+		{"t2", args{a: arg2}, -1},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MaxPathSum(tt.args.a); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MaxPathSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
